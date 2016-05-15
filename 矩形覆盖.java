6ab92278240661e9e2cf85b95a8d4a3adcce5398
username <hcwcyz@gmail.com>
2016-05-15 23:52:17 +0800

    2

---
+++ 矩形覆盖.java
@@ -0,0 +1,14 @@
+public class Solution {
+    public int RectCover(int target) {
+      if(target  <= 1){
+            return 1;
+        }
+        if(target*2 == 2){
+            return 1;
+        }else if(target*2 == 4){
+            return 2;
+        }else{
+            return RectCover((target-1))+RectCover(target-2);
+        }
+    }
+}
